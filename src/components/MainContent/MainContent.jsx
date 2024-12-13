b3d3b2446c63163adf872d16ad9e1ef451a33850
AlexGit40 <oleksandrkolesnyk45@gmail.com>
2024-12-13 17:58:34 +0200

    update navigate

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import MainScreen from "../MainScreen/MainScreen";
 import "./MainContent.scss";
@@ -18,6 +18,7 @@ function MainContent() {
       <Sidebar />
       <MainScreen>
         <Routes>
+          <Route path="/" element={<Navigate to="/use-state" replace />} />
           <Route path="/use-state" element={<UseStateDemo />} />
           <Route path="/use-effect" element={<UseEffectDemo />} />
           <Route path="/use-context" element={<UseContextDemo />} />
